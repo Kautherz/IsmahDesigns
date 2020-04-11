c9e007002a72ced3806ae5a875c1231f125037ae
azaini101 <52187481+azaini101@users.noreply.github.com>
2020-04-11 05:23:36 -0500

    Add files via upload

--- scrollhelper.js
+++ scrollhelper.js
@@ -79,9 +79,37 @@
 
  })();
 
+//Smooth Scroll
+var SmoothScroll = {
+    // Scrolling Core
+    animationTime    : 400, // [ms]
+    stepSize         : 100, // [px]
+
+    // Acceleration
+    accelerationDelta : 50,  // 50
+    accelerationMax   : 3,   // 3
+
+    // Keyboard Settings
+    keyboardSupport   : true,  // option
+    arrowScroll       : 50,    // [px]
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+
+    // Other
+    touchpadSupport   : false, // ignore touchpad by default
+    fixedBackground   : true, 
+    excluded          : ''    
+};
 
 
+// addDomListener(window);
 
-
-
-
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
