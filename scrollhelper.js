8fbee0f0bf5337e45e9b3f74c8feadacb2fd1923
Kautherz <Kautherz@gmail.com>
2020-04-11 03:15:28 -0500

    Add files via upload

--- scrollhelper.js
+++ scrollhelper.js
@@ -79,74 +79,7 @@
 
  })();
 
-//Smooth Scroll
-SmoothScroll({
-    // Scrolling Core
-    animationTime    : 400, // [ms]
-    stepSize         : 100, // [px]
 
-    // Acceleration
-    accelerationDelta : 50,  // 50
-    accelerationMax   : 3,   // 3
-
-    // Keyboard Settings
-    keyboardSupport   : true,  // option
-    arrowScroll       : 50,    // [px]
-
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm   : true,
-    pulseScale       : 4,
-    pulseNormalize   : 1,
-
-    // Other
-    touchpadSupport   : false, // ignore touchpad by default
-    fixedBackground   : true, 
-    excluded          : ''    
-});
-
-// Google Maps
-var myCenter = new google.maps.LatLng(9.9150603, 122.8321918);
-
-function initialize() {
-  var mapProp = {
-    center: myCenter,
-    zoom: 12,
-    scrollwheel: false,
-    draggable: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-  var marker = new google.maps.Marker({
-    position: myCenter,
-  });
-
-  marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-}
-
-// Change style of navbar on scroll
-window.onscroll = function() {
-  myFunction()
-};
-
-function myFunction() {
-  var navbar = document.getElementById("myNavbar");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    navbar.className = "w3-navbar" + " w3-card-2" + " w3-animate-top" + " w3-white";
-  } else {
-    navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
-  }
-}
 
 
 
