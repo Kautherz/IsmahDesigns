0d2210e0dfde6034cbcbc919251079e554623934
azaini101 <52187481+azaini101@users.noreply.github.com>
2020-05-11 04:40:25 -0500

    Add files via upload

--- controllers/game.controllers.js
+++ controllers/game.controllers.js
@@ -1,17 +1,5 @@
 const game = require('../models/game.model');
 class Controllers{
-    getGame(request, response){
-        const id = request.params.id;
-        const data = game.get(id);
-        if(data){
-            data["success"] = true;
-            response.json(data);
-        }
-        else{
-            response.json({"success": false})
-        }
-    }
-
     getProduct(request, response){
         const {productID} = request.query;
         const data = game.get(productID);
@@ -21,6 +9,20 @@ class Controllers{
         }
         else{
             response.json({"success": false})
+        }
+    }
+
+    getGender(request, response){
+        const {gender} = request.query;
+        const {type} = request.query;
+        var data = {};
+        data = game.getGenderDictionary(gender, type);
+        const num_data = Object.keys(data).length;
+        if(num_data == 0){
+            response.json({"success": false});
+        }
+        else{
+            response.json(data);
         }
     }
 }
