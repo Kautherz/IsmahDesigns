3e32f21e20950608b188869425b5495285ca578b
azaini101 <52187481+azaini101@users.noreply.github.com>
2020-05-11 11:31:31 -0500

    Add files via upload

--- product_page.js
+++ product_page.js
@@ -4,13 +4,15 @@ const renderProductPage = async function(){
     const response = await fetch(`https://rocky-refuge-39209.herokuapp.com/product?productID=${product_id}`);
     const product_data = await response.json()
     if(product_data.success){
-        document.getElementById("product-title").innerHTML = product_data.name;
         document.getElementById("product-name").innerHTML = product_data.name;
-        document.getElementById("product-gender").innerHTML = product_data.gender;
         document.getElementById("product-pic1").src = product_data.pictures[0];
         document.getElementById("product-pic2").src = product_data.pictures[1];
-        document.getElementById("product-price").innerHTML = product_data.price;
-        document.getElementById("product-type").innerHTML = product_data.type;
+        document.getElementById("breadcrumb-list").innerHTML = `<ol class="breadcrumb-list" id= "breadcrumb-list">
+        <li class="breadcrumb-item bc"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item bc"><a href="shop.html/products?gender=${product_data.gender}" id="product-gender">${product_data.gender}</a></li>
+        <li class="breadcrumb-item bc"><a href="shop.html/products?gender=${product_data.gender}&type=${product_data.type}" id="product-type">${product_data.type}</a></li>
+        <li class="breadcrumb-item active_breadcrumb", id="product-title">${product_data.name}</li>
+      </ol>`;
     }
     else{
         window.location.replace("error.html");
