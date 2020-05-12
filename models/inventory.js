f94ae65a41a1b1739ba0c923a25367324d72327d
azaini101 <52187481+azaini101@users.noreply.github.com>
2020-05-12 15:56:53 -0500

    Add files via upload

---
+++ models/inventory.js
@@ -0,0 +1,62 @@
+class Inventory{
+    constructor(){
+        this.products = {"123456": {name: "Item 1", type: "Hoodies and Sweatshirts", gender: "Men", pictures: ["images/arbaeen_design2.png", "images/arbaeen_design1.png"], price: 9999.99},
+                    "777777": {name: "Item 2", type: "Hoodies and Sweatshirts", gender: "Men", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 999.99},
+                    "333333": {name: "Item 3", type: "Hoodies and Sweatshirts", gender: "Men", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 99.99},
+                    "444444": {name: "Item 4", type: "Longsleeve T-Shirts", gender: "Men", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 999.90},
+                    "999999": {name: "Item 5", type: "T-Shirts", gender: "Kids", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 99990.99},
+                    "101010": {name: "Item 6", type: "Hoodies and Sweatshirts", gender: "Kids", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 90.99},
+                    "342343": {name: "Item 7", type: "T-Shirts", gender: "Men", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 99.99},
+                    "686532": {name: "Item 8", type: "Hoodies and Sweatshirts", gender: "Women", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 990.99},
+                    "121002": {name: "Item 9", type: "Longsleeve T-Shirts", gender: "Women", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 9999.89},
+                    "111111": {name: "Item 10", type: "Hoodies and Sweatshirts", gender: "Women", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 9999.69},
+                    "122222": {name: "Item 11", type: "T-Shirts", gender: "Women", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 995.99},
+                    "143534": {name: "Item 12", type: "Hoodies and Sweatshirts", gender: "Men", pictures: ["images/muharram_4front.png", "images/muharram_4back.png"], price: 9299.99},
+                    "134323": {name: "Item 13", type: "Hoodies and Sweatshirts", gender: "Kids", pictures: ["images/muharram_5front.png", "images/muharram_5back.png"], price: 909.99}};
+    }
+
+    get(id){
+        if(this.products[id]){
+            const {number, ...data} = this.products[id];
+            return data;
+        }
+        else{
+            return null;
+        }
+    }
+
+    getGenderDictionary(gender, type){
+        var data = {};
+        var keys = Object.keys(this.products);
+        var num_products = Object.keys(this.products).length;
+        if(gender == "Men" || gender == "Women" || gender == "Kids"){
+            if(type == "Hoodies and Sweatshirts" || type == "Longsleeve T-Shirts" || type == "T-Shirts"){
+                var i = 0
+                while(i < num_products){
+                    if(this.products[keys[i]].gender == gender && this.products[keys[i]].type == type){
+                        data[keys[i]] = this.products[keys[i]]; 
+                    }
+                    i++;
+                }
+                return data;
+            }
+            else if(type == null){
+                var i = 0
+                while(i < num_products){
+                    if(this.products[keys[i]].gender == gender){
+                        data[keys[i]] = this.products[keys[i]]; 
+                    }
+                    i++;
+                }
+                return data;
+            }
+            else{
+                return data;
+            }
+        }
+        else{
+            return data;
+        }
+    }
+}
+module.exports = new Inventory();
