470e7e1790b277eaf08177002bc7424b0f835a00
Kautherz <Kautherz@gmail.com>
2020-05-12 16:04:03 -0500

    Add files via upload

--- controllers/game.controllers.js
+++ controllers/game.controllers.js
@@ -1,8 +1,12 @@
-const game = require('../models/inventory');
+const inventory = require('../models/inventory');
+const userBags = require('../models/bag');
+
 class Controllers{
+
+
     getProduct(request, response){
         const {productID} = request.query;
-        const data = game.get(productID);
+        const data = inventory.get(productID);
         if(data){
             data["success"] = true;
             response.json(data);
@@ -12,19 +16,61 @@ class Controllers{
         }
     }
 
-    getGender(request, response){
-        const {gender} = request.query;
-        const {type} = request.query;
-        var data = {};
-        data = game.getGenderDictionary(gender, type);
-        const num_data = Object.keys(data).length;
-        if(num_data == 0){
-            response.json({"success": false});
+    addBag(request, response){
+       // console.log(request.session.id);
+        //console.log(request.body.productIDs);
+    
+        const sessionID = request.session.id;
+        const {products} = request.body;
+        const bag = userBags.add(sessionID, products);
+        console.log(sessionID, bag);
+
+
+        if (bag){
+            //userBags[sessionID].push(productIDs); 
+            response.json({'success': true, 'bag': bag });
         }
         else{
-            response.json(data);
+           response.json({'success': false});
         }
     }
+
+    getBag(request, response){
+        //console.log(request.session.id);
+        //response.json({success:true})
+        
+        const sessionID = request.session.id;
+
+        const bag = userBags.get(sessionID);
+        console.log(sessionID, bag);
+
+        if(bag){
+            response.json({'success': true, 'bag': bag });
+        }
+        else{
+             response.json({"success": false})
+
+        }
+    }
+
+    setBag(request, response){
+
+        const sessionID = request.session.id;
+        const {products} = request.body;
+        const bag = userBags.set(sessionID, products);
+
+
+        if (bag){
+            //userBags[sessionID].push(productIDs); 
+            response.json({'success': true, 'bag': bag });
+        }
+        else{
+           response.json({'success': false});
+        }
+
+
+    }
+    
 }
 
 module.exports = new Controllers();
