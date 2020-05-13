9e59f44ad12c70fc0e533f0dad52c3bf56261fe2
Kautherz <Kautherz@gmail.com>
2020-05-13 02:15:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,27 +7,10 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 function setupApp(){
-	app.use(cors());
+	app.use(cors({credentials: true, origin: true}));
    // app.use(localHostHandler);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:false}));
-
-    //app.use(session({secret: 'secret-word', resave: false, saveUninitialized: true, cookie: { secure: false, httpOnly: false }}))
-
-   // var sess = {
-    //	secret: 'secret-word',
-    //	cookie: {}
-    //}
-
-    //if (app.get('env') === 'production') {
-    //	app.set('trust proxy', 1) // trust first proxy
-    	//sess.cookie.secure = false // serve secure cookies
-	//}
-
-	//app.use(session(sess))
-
-    //app.use(session({cookie: {secure: false}})); 
-
     const sessionConfig = { secret:'secret-word', resave:false, saveUninitialized:true };
     app.use(session(sessionConfig) );
     app.use('/', routes);  
